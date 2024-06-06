eba88d7aa672e97eafe128eb28c89a2539c7c2a6
Bert De Block <bert.deblock@bagaar.be>
2024-06-06 13:20:12 +0200

    Import SimpleBar styling via component file

--- addon/components/simple-bar.js
+++ addon/components/simple-bar.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import SimpleBarCore from 'simplebar-core';
+import 'simplebar-core/dist/simplebar.css';
 
 export default class SimpleBarComponent extends Component {
   @tracked
--- index.js
+++ index.js
@@ -2,9 +2,4 @@
 
 module.exports = {
   name: require('./package').name,
-
-  included(app) {
-    this._super.included.apply(this, arguments);
-    app.import('node_modules/simplebar-core/dist/simplebar.css');
-  },
 };
